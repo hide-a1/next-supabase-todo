f8d990688f924c448fa61d0ca0502b7ba1655a1a
hide-a1 <hide210g@gmail.com>
2023-01-23 22:48:49 +0900

    feat: auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,64 @@
 import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { supabase } from '@/utils/supabase'
+import type { AppProps, NextWebVitalsMetric } from 'next/app'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  switch (metric.name) {
+    case 'FCP':
+      console.log(`FCP: ${Math.round(metric.value * 10) / 10}`)
+      break
+    case 'LCP':
+      console.log(`LCP: ${Math.round(metric.value * 10) / 10}`)
+      break
+    case 'TTFB':
+      console.log(`TTFB: ${Math.round(metric.value * 10) / 10}`)
+      break
+    case 'Next.js-hydration':
+      console.log(
+        `Hydration: ${Math.round(metric.startTime * 10) / 10} -> ${
+          Math.round((metric.startTime + metric.value) * 10) / 10
+        }`
+      )
+      break
+  }
+}
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { push, pathname } = useRouter()
+  const validateSession = async () => {
+    const user = supabase.auth.user()
+    if (user && pathname === '/') {
+      push('/dashboard')
+    } else if (!user && pathname !== '/') {
+      await push('/')
+    }
+  }
+  supabase.auth.onAuthStateChange((event, _) => {
+    if (event === 'SIGNED_IN' && pathname === '/') {
+      push('/dashboard')
+    }
+    if (event === 'SIGNED_OUT') {
+      push('/')
+    }
+  })
+  useEffect(() => {
+    validateSession()
+  }, [])
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
 }
