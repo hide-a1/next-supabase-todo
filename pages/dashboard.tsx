d77884f944937a23c32dc42e3f570b18ce2e2e15
hide-a1 <hide210g@gmail.com>
2023-01-24 22:29:46 +0900

    feat: added RLS

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,11 +1,23 @@
 import { Layout } from '@/components/Layout'
+import { NoticeForm } from '@/components/NoticeForm'
+import { NoticeList } from '@/components/NoticeList'
+import { TaskForm } from '@/components/TaskForm'
+import { TaskList } from '@/components/TaskList'
 import { supabase } from '@/utils/supabase'
-import { LogoutIcon } from '@heroicons/react/solid'
+import {
+  DocumentTextIcon,
+  LogoutIcon,
+  StatusOnlineIcon,
+} from '@heroicons/react/solid'
 import { NextPage } from 'next'
+import { useQueryClient } from 'react-query'
 
 const Dashboard: NextPage = () => {
+  const queryClient = useQueryClient()
   const signOut = () => {
     supabase.auth.signOut()
+    queryClient.removeQueries('todos')
+    queryClient.removeQueries('notices')
   }
   return (
     <Layout title="Dashboard">
@@ -13,6 +25,22 @@ const Dashboard: NextPage = () => {
         className="mb-6 h-6 w-6 cursor-pointer text-blue-500"
         onClick={signOut}
       />
+      <div className="grid grid-cols-2 gap-40">
+        <div>
+          <div className="my-3 flex justify-center">
+            <DocumentTextIcon className="h-8 w-8 text-blue-500" />
+          </div>
+          <TaskForm />
+          <TaskList />
+        </div>
+        <div>
+          <div className="my-3 flex justify-center">
+            <StatusOnlineIcon className="h-8 w-8 text-blue-500" />
+          </div>
+          <NoticeForm />
+          <NoticeList />
+        </div>
+      </div>
     </Layout>
   )
 }
